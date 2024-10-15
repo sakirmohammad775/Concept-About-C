095f26d0f6145699b0b85b682ea1757154388f5a
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-10-15 14:46:07 +0600

    first commit

---
+++ password.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    
+    
+    while (scanf("%d", &n)!= EOF)
+        // if (n == 1990)
+        // {
+        //     printf("Right");
+        // }
+        // else
+        // {
+        //     printf("Wrong");
+        // }
+        return 0;
+}
