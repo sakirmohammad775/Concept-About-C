50f872fd7d6725bb2d17296c7dcbb3cc3b0410e3
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-10-15 14:17:31 +0600

    first commit

---
+++ concept1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int secondDigit = n % 10;
+    int firstDigit = n / 10;
+    if(secondDigit==0){
+        printf("No");
+        return 0;  // If the second digit is 0, the result can't be divisible by the first digit. So, no need to check further.  // added this line to optimize the code.
+    }
+    else if (secondDigit % firstDigit==0 || firstDigit % secondDigit ==0)
+    {
+        printf("Yes");
+    }
+    else
+    {
+        printf("No");
+    }
+    return 0;
+}
+
+// 39 output yes
+// 63 output no
