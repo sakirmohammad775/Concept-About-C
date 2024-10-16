cf41ee43e4b82ea959ce35e445799e18762926af
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-10-17 01:15:14 +0600

    add to the git profile

---
+++ W_Mathematical.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main()
+{
+    int A, B, C;
+    char S, Q;
+    scanf("%d %c %d %c %d", &A, &S, &B, &Q, &C);
+    
+
+    if (S == '+') {
+        if (A + B == C) {
+            printf("Yes");
+        } else {
+            printf("%d", A + B);
+        }
+    }
+    else if (S == '-') {
+        if (A - B == C) {
+            printf("Yes");
+        } else {
+            printf("%d", A - B);
+        }
+    }
+    else if (S == '*')
+    {
+        
+        if(A*B== C){
+            printf("Yes");
+        
+        }
+        else {
+            printf("%d",A*B);
+
+        }
+        
+    }
+    return 0;
+}
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    int A, B, C;
+    char S, Q;
+    scanf("%d %c %d %c %d", &A, &S, &B, &Q, &C);
+    printf("%d %c %d %c %d\n", A, S, B, Q, C);
