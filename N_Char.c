789365a811548925f1dc10ed5f37aadac0c54229
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-10-16 23:08:06 +0600

    add to github

--- N_Char.c
+++ N_Char.c
@@ -1,1 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
+int main()
+{
+    char ch;
+    scanf("%c", &ch);
+    if ('a' <= ch && ch <= 'z'){
+        ch-=32;
+        //lower case letter
+    }
+    else{
+        ch+=32;
+        //upper case letter
+    }
+    printf("%c\n",ch);
+    return 0;
+}
+//for lower case letter => 32 subtraction from letter
+//for upper case letter => 32 summation with letter
---
+++ V_Comparison.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int A, B;
+    char s;
+    scanf("%d %c %d", &A, &s, &B);
+    printf("%d %c %d\n", A, s, B);
+
+    if(s=='>' && ){
+
+    }
+
+    return 0;
+} 
