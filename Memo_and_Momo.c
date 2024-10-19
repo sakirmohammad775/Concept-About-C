0f664662194f47b337c90b368cbe8acd401003a8
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-10-19 22:18:12 +0600

    add  memo and momo

---
+++ Memo_and_Momo.c
@@ -0,0 +1,33 @@
+// Memo and Momo are playing a game. Memo will choose a positive number a
+// , and Momo will choose a positive number b
+// .
+
+ // Your task is to tell them who will win according to the following rules:
+
+// If both a
+//  and b
+//  are divisible by k
+// , both of them win and you should print "Both".
+// If a
+//  is divisible by k
+//  but b
+//  isn't, Memo wins and you should print "Memo".
+// If b
+//  is divisible by k
+//  but a
+//  isn't, Momo wins and you should print "Momo".
+// If both a
+//  and b
+//  are not divisible by k
+// , no one wins and you should print "No One".
+
+
+
+
+
+
+
+
+
+
+
