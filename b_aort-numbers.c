c374bf5f539fb0ab57b250428a8058f74ddd59a8
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-10-19 23:48:10 +0600

    add  memo and momo

--- b_aort-numbers.c
+++ b_aort-numbers.c
@@ -4,20 +4,14 @@ int main(){
     int a,b,c;
     scanf("%d %d %d",&a,&b,&c);
     if(a>b && b>c ){
-        printf("%d",a);
-        printf(" %d",b);
-        printf(" %d",c);
+        printf("%d\n %d\n %d\n",a,b,c);
+        
     }
     else if (b>a &&  b>c){
-        printf("%d",b);
-        printf(" %d",c);
-        printf(" %d",a);
+       printf("%d\n%d\n%d\n",b,c,a);
+        
     }
-    else{
-        printf("%d",c);
-        printf(" %d",b);
-        printf(" %d",a);
-    }
+   
     
     return 0;
 }
