fd79b13f612616c87f3fb8d832f707c7eaac35f8
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-10-20 11:09:24 +0600

    add G sum of 1 to N

--- G_Sum_of_1_to_N.c
+++ G_Sum_of_1_to_N.c
@@ -13,3 +13,28 @@ Print the summation of the numbers that are between 1 and N
  (inclusive).
  
 */
+#include<stdio.h>
+int main(){
+    long long n;
+    scanf("%lld",&n);
+    printf("%lld\n",(n*(n+1))/2);  // formula to find sum of first n natural numbers
+    return 0;
+}
+//OR Solve two way
+ //1. using formula for summation of first n natural numbers
+ //2. using simple loop to add numbers from 1 to n and print the sum
+ //3. using formula for summation of first n natural numbers (n*(n+1))/2
+ //4. using simple loop to add numbers from 1 to n and print the sum
+ //5. using formula for summation of first n natural numbers (n*(n+1))/2
+ //6. using simple loop to add numbers from 1 to n and print the sum
+#include<stdio.h>
+int main(){
+    long long n;
+    scanf("%lld",&n);
+    long long sum=0;
+    for(int i=0;i<=n;i++){
+        sum+=i;
+    }
+        printf("%lld ",sum);
+    return 0;
+}
