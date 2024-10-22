6eb1d1b7bbdae44c0f1716874fe1b96fd3065a9a
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-10-22 23:06:41 +0600

    add the factorial

--- G_Factorial.c
+++ G_Factorial.c
@@ -5,7 +5,18 @@ int main()
     int cn; // case number input
     scanf("%d", &cn);
     for (int cs = 0; cs < cn; cs++) // for loop ,How many numbers in the n;
-   
+    {
+        int n;
+        scanf("%d", &n);
+
+        long long fact = 1;
+        for (int i = 1; i <= n; i++) // for loop for factorial
+
+        {
+            fact *= i;
+        }
+        printf("%lld\n", fact);
+    }
 
     return 0;
 }
