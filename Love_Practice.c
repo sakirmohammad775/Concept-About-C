5a631ae3c3543a4391e9f5f37cf41d6981ba2ab8
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-10-29 23:42:47 +0600

    vaariable justify

--- Love_Practice.c
+++ Love_Practice.c
@@ -1,10 +1,29 @@
-#include<stdio.h>
-int main(){
-    int x,y;
-    scanf("%d %d",&x,&y);
-    
-    printf("%d\n",x+y);
+#include <stdio.h>
 
-    
+int main() {
+    // Declare variables
+    int intValue;
+    long long longValue;  // Ensure we use long long for large integers
+    double doubleValue;
+    char charValue;
+
+    // Read all input values in one line
+    // Make sure to consume the whitespace before reading the character
+    scanf("%d %lld %lf %c", &intValue, &longValue, &doubleValue, &charValue);
+
+    // Print the output in one line
+    printf("%d \n%lld \n%.2f \n%c\n", intValue, longValue, doubleValue, charValue);
+
     return 0;
 }
+/*
+input 100
+1234567891234567
+23.5675
+A
+output
+100 
+1234567891234567 
+23.57 
+A
+*/
